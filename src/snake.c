52848654bcd734e70ac052b9438cec2ad316cdde
Villy-P <valeriuspetrini@gmail.com>
2024-09-09 16:15:53 -0500

    fix: not setting ocrrect memory with malloc

--- src/snake.c
+++ src/snake.c
@@ -8,7 +8,7 @@
 struct Snake* generateSnake(struct Segment *head) {
     struct Snake *snake = (struct Snake*)malloc(sizeof(struct Snake));
     snake->length = 1;
-    snake->segments = malloc(sizeof(struct Segment) * snake->length);
+    snake->segments = malloc(sizeof(struct Segment*) * snake->length);
     snake->segments[0] = head;
     snake->direction = RIGHT;
     return snake;
