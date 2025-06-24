aa1a69e7061c6d8095ebec1d0b9d9ba22d24741e
Villy-P <valeriuspetrini@gmail.com>
2025-06-24 15:48:28 -0500

    refactor: improve indentation and structure of wasKeyDown function for readability

--- src/util.c
+++ src/util.c
@@ -4,9 +4,21 @@
 #include <stdlib.h>
 
 int wasKeyDown(int key) {
+    #ifdef __WIN32
     if (0x00000001 & GetAsyncKeyState(key))
         return 1;
     return 0;
+    #else
+    Display *display = XOpenDisplay(NULL);
+    if (display == NULL) {
+        fprintf(stderr, "Unable to open X display\n");
+        return 0;
+    }
+    char keys[32];
+    XQueryKeymap(display, keys);
+    KeyCode keycode = XKeysymToKeycode(display, key);
+    return (keys[keycode >> 3] & (1 << (keycode & 7))) != 0;
+    #endif
 }
 
 int randrange(int min, int max){
